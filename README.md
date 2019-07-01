16aebd822a06026f10485ccff28ee3d29d35cb3e
yude268 <yude268@gmail.com>
2019-07-01 22:55:35 +0800

    Signed-off-by: yude268 <yude268@gmail.com>

--- README.md
+++ README.md
@@ -1,7 +1,63 @@
-# onekey_caddy_proxy
+# 一键搭建基于 caddy 的 https(h2) 代理
 
 
+## 1.一键安装（随机密码 自带临时域名）
+
+```
 bash <(curl -L -s git.io/a.sh)
+```
+
+## 2.一键安装（自定义账号密码 自定义域名）
+
+```
+格式：
+
+bash <(curl -L -s git.io/a.sh) 用户名 密码 你的域名 要伪装的网站
+
+举例：
+
+bash <(curl -L -s git.io/a.sh) admin 888888 www.yourdomian.com www.apple.com
+```
+
+## 3.一键卸载
+
+```
+bash <(curl -L -s git.io/a.sh) uninstall
+```
+
+客户端配置
+
+```
+- Chrome:     ProxySwitchyOmega 插件，代理类型选择 https, 端口填443, 再点击右侧的小锁，输入用来验证代理的用户名和密码即可。
+- Firefox:    Foxyproxy 插件，配置方式大同小异。
+- IOS:        SURGE 等，代理类型选择HTTPS，配置方式大同小异。
+- Android:    ProxyDroid、Postern 等，配置方式大同小异。
+```
+
+可能用到的命令
+
+```
+Debian8 更新系统 安装必要软件：
+
+apt update -y
+apt install curl -y
+
+强行释放被占用的端口 以80端口为例：
+
+CMD=`lsof -i:"80" | awk '{print $1}' | grep -v "COMMAND" | sort -u` && systemctl disable ${CMD} && systemctl stop ${CMD} && killall -9 ${CMD}
+
+Debian8 关闭 apache2 ：
+
+systemctl stop apache2
+systemctl disable apache2
+
+```
+
+## 关联项目：
+
+https://c2ray.ml
+
+https://github.com/dylanbai8/c2ray
 
 
-bash <(curl -L -s git.io/a.sh) yude268 130130 youdiangan.ga www.adboys.cn
+
--- a.sh
+++ a.sh
@@ -29,7 +29,7 @@ if [ ! ${user} ]; then
 user="admin"
 fi
 
-#设置默认密码
+#设置默认随机密码
 if [ ! ${pass} ]; then
 pass=`cat /dev/urandom | head -n 10 | md5sum | head -c 8`
 fi
@@ -41,9 +41,24 @@ echo `curl -4 ip.sb` >> local_ip.txt && sed -i "s/\./\-/g" "local_ip.txt"
 domain="$(cat local_ip.txt).ip.c2ray.ml" && rm -rf local_ip.txt
 fi
 
-#设置默认伪装网站
-if [ ! ${website} ]; then
+#设置默认随机伪装网站
+sitenum="shuf -n 1 -e 1 2 3 4 5 6 7 8"
+if [[ ! ${website} ]] && [[ ${sitenum} -eq 1 ]]; then
+website="www.ibm.com"
+elif [[ ! ${website} ]] && [[ ${sitenum} -eq 2 ]]; then
 website="www.stenabulk.com"
+elif [[ ! ${website} ]] && [[ ${sitenum} -eq 3 ]]; then
+website="www.qualcomm.com"
+elif [[ ! ${website} ]] && [[ ${sitenum} -eq 4 ]]; then
+website="tw.longchamp.com"
+elif [[ ! ${website} ]] && [[ ${sitenum} -eq 5 ]]; then
+website="www.apple.com"
+elif [[ ! ${website} ]] && [[ ${sitenum} -eq 6 ]]; then
+website="www.rodesk.com"
+elif [[ ! ${website} ]] && [[ ${sitenum} -eq 7 ]]; then
+website="www.adidas.com.cn"
+elif [[ ! ${website} ]] && [[ ${sitenum} -eq 8 ]]; then
+website="www.frontlynk.com"
 fi
 
 }
@@ -59,8 +74,6 @@ echo "----------------------------------------------------------"
 
 rm -rf /usr/local/bin/Caddyfile
 
-rm -rf /usr/local/bin/caddy
-
 }
 
 
@@ -68,11 +81,21 @@ rm -rf /usr/local/bin/caddy
 #安装caddy
 install_caddy(){
 
+if [[ -e /usr/local/bin/caddy ]]; then
+
+echo "----------------------------------------------------------"
+echo "检测到caddy已安装跳过安装程序"
+echo "----------------------------------------------------------"
+
+else
+
 echo "----------------------------------------------------------"
 echo "正在安装caddy主程序和代理相关插件"
 echo "----------------------------------------------------------"
 
 curl https://getcaddy.com | bash -s personal http.forwardproxy,http.proxyprotocol
+
+fi
 
 }
 
@@ -155,7 +178,7 @@ mv ./*${website}*/* /www
 restart_caddy(){
 
 echo "----------------------------------------------------------"
-echo "重启caddy载入配置文件"
+echo "正在重启caddy载入配置文件"
 echo "----------------------------------------------------------"
 
 systemctl daemon-reload
@@ -171,7 +194,7 @@ show_proxy_info(){
 clear
 echo "----------------------------------------------------------"
 echo ""
-echo "代理协议：Https"
+echo "代理协议：https"
 echo ""
 echo "代理服务器：${domain}"
 echo "代理端口：${port}"
@@ -197,6 +220,26 @@ show_proxy_info
 }
 
 
+
+if [ ${user} = uninstall ]; then
+
+systemctl stop caddy
+systemctl disable caddy
+
+rm -rf /usr/local/bin/Caddyfile
+rm -rf /usr/local/bin/caddy
+rm -rf /etc/systemd/system/caddy.service
+
+fi
+
+clear
+echo "----------------------------------------------------------"
+echo ""
+echo "caddy已卸载"
+echo ""
+echo "关联项目：https://c2ray.ml"
+echo ""
+echo "----------------------------------------------------------"
 
 main
 
