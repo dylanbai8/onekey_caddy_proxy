382803d6d5596ef06d9746216f99fb39b8660103
yude268 <yude268@gmail.com>
2019-07-01 14:53:57 +0800

    Signed-off-by: yude268 <yude268@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # onekey_caddy_proxy
+
+
+bash <(curl -L -s git.io/a.sh)
--- a.sh
+++ a.sh
@@ -1,116 +1,159 @@
 #!/bin/bash
 
 #====================================================
-# V2ray for mKCP+uTP BT伪装 [ debian 8 ]
+# 一键搭建基于 caddy 的 https(h2) 代理 [ debian 8 ]
 #====================================================
 
-update_system(){
-apt-get update -y
-apt-get install curl -y
-apt-get install cron -y
 
-echo ""
-echo "----------------------------------------------------------"
-echo "系统更新完成、安装 curl cron 完成"
-echo "----------------------------------------------------------"
+
+#设置用户名
+set_proxy_info(){
+
+user="$1"
+
+[[ -z ${user} ]] && user="username"
+
+pass="$2"
+
+[[ -z ${pass} ]] && pass="password"
+
+domain="$3"
+port="443"
+
+touch local_ip.txt && echo `curl -4 ip.sb` >> local_ip.txt && sed "s/./-/" local_ip.txt
+[[ -z ${domain} ]] && domain="$(cat local_ip.txt).ip.c2ray.ml:${port}" && rm -rf local_ip.txt
+
+website="$4"
+
+[[ -z ${website} ]] && website="www.stenabulk.com"
+
 }
 
-install_v2ray(){
-bash <(curl -L -s https://install.direct/go.sh)
 
-echo ""
-echo "----------------------------------------------------------"
-echo "V2ray安装完成"
-echo "----------------------------------------------------------"
+
+#清除可能残余的caddy
+clean_caddy(){
+
+rm -rf /usr/local/bin/Caddyfile
+
+rm -rf /usr/local/bin/caddy
+
 }
 
-set_config(){
-touch /etc/v2ray/config.json
-cat <<EOF > /etc/v2ray/config.json
-{
-  "inbounds": [
-    {
-      "port": 10303,
-      "protocol": "vmess",
-      "settings": {
-        "clients": [
-          {
-            "id": "78266cb5-8860-4d12-9095-296f784d4891",
-            "alterId": 72
-          }
-        ]
-      },
-      "streamSettings": {
-        "network": "mkcp",
-        "kcpSettings": {
-          "uplinkCapacity": 5,
-          "downlinkCapacity": 100,
-          "congestion": true,
-          "header": {
-            "type": "utp"
-          }
-        }
-      }
-    }
-  ],
-  "outbounds": [
-    {
-      "protocol": "freedom",
-      "settings": {}
-    }
-  ]
+
+
+#安装caddy
+install_caddy(){
+
+curl https://getcaddy.com | bash -s personal http.forwardproxy,http.proxyprotocol
+
+}
+
+
+
+#配置caddy
+config_caddy(){
+
+touch /usr/local/bin/Caddyfile
+
+cat <<EOF > /usr/local/bin/Caddyfile
+${domain}{
+tls admin@${domain}
+root /www
+gzip
+index index.html
+forwardproxy {
+    basicauth ${user} ${pass}
+}
 }
 EOF
 
-echo ""
-echo "----------------------------------------------------------"
-echo "V2ray配置完成"
-echo "----------------------------------------------------------"
 }
 
-restart(){
-service v2ray restart
 
-echo ""
-echo "----------------------------------------------------------"
-echo "V2ray载入配置完成"
-echo "----------------------------------------------------------"
+
+#开机自启动caddy
+auto_caddy(){
+
+touch /etc/systemd/system/caddy.service
+
+cat <<EOF > /etc/systemd/system/caddy.service
+[Unit]
+Description=Caddy_Server
+After=network.target
+Wants=network.target
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/caddy -conf=/usr/local/bin/Caddyfile -agree=true -ca=https://acme-v02.api.letsencrypt.org/directory
+RestartPreventExitStatus=23
+Restart=always
+User=root
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable caddy
+
 }
 
-information(){
+
+
+#安装伪装网站
+website_caddy(){
+
+rm -rf /www && mkdir /www
+
+wget -c -r -np -k -L -p ${website}
+
+mv ./*${website}*/* /www
+
+}
+
+
+
+#重启caddy
+restart_caddy(){
+
+systemctl daemon-reload
+
+systemctl restart caddy
+
+}
+
+
+
+#展示配置信息
+show_proxy_info(){
 clear
 echo "----------------------------------------------------------"
-echo "Port: 10303"
-echo "UUID: 78266cb5-8860-4d12-9095-296f784d4891"
-echo "alterId: 72"
-echo "network: mkcp"
-echo "type: utp"
-echo "----------------------------------------------------------"
-}
-
-auto_restart_update(){
-crontab -l >> crontab.txt >/dev/null 2>&1
-echo "10 12 * * 1 bash <(curl -L -s https://install.direct/go.sh)" >> crontab.txt
-echo "30 12 * * * /sbin/reboot" >> crontab.txt
-echo "30 * * * * service v2ray restart" >> crontab.txt
-crontab crontab.txt
-systemctl restart cron
-rm -f crontab.txt
-
 echo ""
-crontab -l
-echo "----------------------------------------------------------"
-echo "已设置：每周一升级 V2ray、每天凌晨重启服务器"
+echo "代理协议：Https"
+echo ""
+echo "代理服务器：${domain}"
+echo "代理端口：${port}"
+echo ""
+echo "用户名：${user}"
+echo "密码：${pass}"
+echo ""
 echo "----------------------------------------------------------"
 }
 
+
+
+#命令执行列表
 main(){
-update_system
-install_v2ray
-set_config
-restart
-information
-auto_restart_update
+set_proxy_info
+clean_caddy
+install_caddy
+config_caddy
+auto_caddy
+website_caddy
+restart_caddy
+show_proxy_info
 }
+
+
 
 main
+
+
