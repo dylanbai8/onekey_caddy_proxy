06eb4467d16916b301f3928ebddb04f71df75425
yude268 <yude268@gmail.com>
2019-07-06 10:56:02 +0800

    Signed-off-by: yude268 <yude268@gmail.com>

--- a.sh
+++ a.sh
@@ -408,24 +408,8 @@ fi
 #输出进度条
 wait_ssl_procing(){
 
-COUNTER=0 
-_R=0 
-_C=`tput cols`  
-_PROCEC=`tput cols`  
-tput cup $_C $_R  
-printf "["  
-while [ $COUNTER -lt 100 ]  
-do  
-    COUNTER=`expr $COUNTER + 1`  
-    sleep 0.1  
-    printf "=>"  
-    _R=`expr $_R + 1`  
-    _C=`expr $_C + 1`  
-    tput cup $_PROCEC 101  
-    printf "]%d%%" $COUNTER  
-    tput cup $_C $_R  
-done  
-printf "\n"
+echo -en '[ #                    ]     05%\r'
+echo -en '[ ###################  ]     95%'
 
 }
 
